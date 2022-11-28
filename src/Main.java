fb0bbacd010a0541c43c5ff5b3fd5b64bd4ce839
Рустам <rustam19871203@gmail.com>
2022-11-28 14:08:41 +0400

    Коммит

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, String> directory = new  HashMap();
+        directory.put("Николай Петрович", "+79256314587");
+        directory.put("Петр Николаевич", "+79254523621");
+        directory.put("Иван Васильевич", "+79258965425");
+        directory.put("Василий Иванович", "+79257854123");
+        directory.put("Андрей Алексеевич", "+79259635214");
+        directory.put("Алексей Андреевич", "+79258596874");
+        directory.put("Павел Сергеевич", "+79251233212");
+        directory.put("Сергей Павлович", "+79257771122");
+        directory.put("Антон Филипович", "+79259685412");
+        directory.put("Филип Антонович", "+79258523247");
+        directory.put("Вадим Денисович", "+79258967841");
+        directory.put("Денис Вадимович", "+79255231211");
+        directory.put("Инокентий Яковлевич", "+79258526669");
+        directory.put("Яков Инокентьевич", "+79251501119");
+        directory.put("Светлана Николаевна", "+79258549573");
+        directory.put("Елена Петровна", "+792565478598");
+        directory.put("Жанна Антоновна", "+79257139564");
+        directory.put("Серафима Сергеевна", "+7925698521");
+        directory.put("Елизавета Филиповна", "+7925729864");
+        directory.put("Антонина Денисовна", "+79259876542");
+
+        System.out.println(directory.values());
+    }
+}
